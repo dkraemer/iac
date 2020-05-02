da1273e4787dc67eb277e780d67ab01f42688e18
Daniel Kraemer <dkraemer@dkross.org>
2020-05-02 22:03:25 +0200

    Project creation

---
+++ README.md
@@ -0,0 +1,35 @@
+# Infrastructure as Code using the [HashiCorp Stack](https://www.hashicorp.com/#overview)
+A collection of templates to automate my virtual test environment.
+
+### Required software
+- [Oracle VirtualBox 6.1.6](https://www.virtualbox.org/wiki/Downloads)
+- [HashiCorp packer 1.5.5](https://www.packer.io/downloads/)
+
+### Creating the required base VM
+This is required until I decide to preseed the installation. (Which sucks BTW)
+1. Download [Ubuntu 18.04 (Bionic) mini.iso](http://archive.ubuntu.com/ubuntu/dists/bionic-updates/main/installer-amd64/current/images/netboot/mini.iso)
+2. Create a VirtualBox VM and connect mini.iso.
+3. Boot the VM and install Ubuntu. The Task `OpenSSH Server` must be selected!
+4. Shutdown the VM and create a snapshot for packer.
+
+### Project structure
+- [Settings](.vscode) for [Visual Studio Code](https://code.visualstudio.com/)
+- [Files](packer) for packer (build and provision)
+  - [Minimal Ubuntu 18.04 LTS base image](packer/base-ubuntu-18.04)
+    - Variables:
+      - `vm_name`: Name of the VM created in step 2. (default: `base-ubuntu-18.04`)
+      - `vm_attach_snapshot`: Name of the snapshot created in step 4 (default: `base`).
+      - `vm_target_snapshot`: Name of the snapshot that is created by packer (default: `packed`).
+      - `vm_user`: Name of the user created during Ubuntu installation (default: `ubuntu`).
+      - `vm_password`: User's password (default: `ubuntu`).
+    - Builders:
+      - [virtualbox-vm](https://www.packer.io/docs/builders/virtualbox/vm/)
+        - Communicator: [ssh](https://www.packer.io/docs/communicators/ssh/)
+    - Provisioners:
+      - [shell](https://www.packer.io/docs/provisioners/shell/)
+    - Post-processors:
+      - [vagrant](https://www.packer.io/docs/post-processors/vagrant/) (not implemented yet)
+
+
+
+
---
+++ packer/base-ubuntu-18.04/provision.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+apt_get="sudo DEBIAN_FRONTEND=noninteractive apt-get"
+apt_get_install="${apt_get} install -y --no-install-recommends"
+
+### Unlock sudo and add provision sudoers file
+echo $1 | sudo -S echo "sudo unlocked"
+echo "$USER ALL=(ALL) NOPASSWD:ALL" | (sudo su -c 'EDITOR="tee -a" visudo -f /etc/sudoers.d/provision')
+
+### Remove password and disable password login
+sudo passwd -d $USER
+sudo passwd -l $USER
+
+### Enable tracing AFTER using password operations
+set -o xtrace
+
+### Update & upgrade packages
+$apt_get update
+$apt_get upgrade -y
+
+### All paths are relative to HOME
+cd $HOME
+
+### Install VirtualBox guest additions
+$apt_get_install make gcc perl
+sudo mount -v VBoxGuestAdditions.iso /media -o loop,ro
+set +o errexit
+sudo /media/VBoxLinuxAdditions.run
+set -o errexit
+sudo umount /media
+rm -v -f VBoxGuestAdditions.iso
+
+### Install default grub config
+sudo mv -v /etc/default/grub /etc/default/grub.ubuntu
+sudo install -v -m 644 grub /etc/default/
+rm -v -f grub
+sudo update-grub
+
+### Remove swapfile
+sudo swapoff /swapfile
+sudo rm -v -f /swapfile
+sudo sed -e 's;^/swapfile.*;;' -i /etc/fstab
+
+### Setup /tmp as tmpfs
+echo 'tmpfs /tmp tmpfs defaults 0 0' | (sudo su -c 'tee -a /etc/fstab')
+sudo rm -v -r -f /tmp
+sudo mkdir -v -m 1777 /tmp
+sudo mount -v /tmp
+
+### Setup SSH
+sudo sed -e 's/^#\(PermitRootLogin\).*/\1 no/' -e 's/^#\(PasswordAuthentication\).*/\1 no/' -i /etc/ssh/sshd_config
+chmod -v 600 authorized_keys
+mkdir -v -m 700 .ssh
+mv -v authorized_keys .ssh/
+
+### Install .bash_aliases
+chmod -v 644 .bash_aliases
+sudo cp -v .bash_aliases /etc/skel/
+sudo cp -v .bash_aliases /root/
+
+### Install addtional packages
+$apt_get_install zerofree
+
+### Purge unwanted packages
+$apt_get remove -y --purge plymouth linux-firmware
+
+### Purge unneeded auto packages
+$apt_get autoremove -y --purge
+
+### Delete this script
+rm -v -f "${0}"
